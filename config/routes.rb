8c029f154d1093badf1e31e277bd3ce4513f6998
Sebastian Armano <sebarmano@gmail.com>
2014-06-19 17:18:20 -0400

    Created assignments and students resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :students
+
+  resources :assignments
+
   resources :teachers
 
   # The priority is based upon order of creation: first created -> highest priority.
